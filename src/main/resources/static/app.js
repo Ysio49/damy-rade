4ed9dd55ab6b1ed566805042949e1fec8e7fe8a5
marekszk <marek.szkudelski@gmail.com>
2017-05-20 00:18:16 +0200

    działający routing

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,29 @@
-var code4life = angular.module('code4life', ['ngMaterial']);
+var code4life = angular.module('code4life', ['ngMaterial', 'ngRoute']);
 
 code4life.controller('testCtrl', ['$scope', function ($scope) {
-
-    $scope.test = "hello world";
+    var vm = this;
+    vm.test = "hello world";
 }]);
+
+code4life.config(function ($routeProvider) {
+
+  /**
+   * $routeProvider
+   */
+  path = "/templates/pages/";
+
+  $routeProvider
+  .when('/', {
+    templateUrl: 'dashboard.html',
+    controller: 'testCtrl'
+  })
+  .when('/test', {
+    templateUrl: 'test.html',
+    controller: 'testCtrl'
+  })
+  .otherwise({
+    redirectTo: '/',
+    controller: 'testCtrl'
+  });
+
+});
