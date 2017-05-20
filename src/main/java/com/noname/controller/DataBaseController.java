72cd09e4dbacf3a13dc438a16cd42449dbdba5c6
marekszk <marek.szkudelski@gmail.com>
2017-05-20 10:13:40 +0200

    wyszukiwanie po mieście ofert

--- src/main/java/com/noname/controller/DataBaseController.java
+++ src/main/java/com/noname/controller/DataBaseController.java
@@ -68,4 +68,5 @@ public class DataBaseController {
         return jobOfferRepository.findAll();
     }
 
+
 }
--- src/main/resources/static/ctrls/search.js
+++ src/main/resources/static/ctrls/search.js
@@ -1,7 +1,17 @@
 
 angular.module('code4life.search', ['ngRoute'])
 
-code4life.controller('searchCtrl', [ '$scope', '$routeParams', function($scope, $routeParams) {
-    var city = $routeParams.city;
-    console.log(city, "city");
+code4life.controller('searchCtrl', [ '$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    var city = $routeParams.city; 
+    $scope.offers = [];
+    $http({
+        method: 'GET',
+        url: 'api/getAllOffer/'
+    }).then(function successCallback(response) {
+        $scope.offers = response.data;
+        console.log($scope.offers, "$scope.offers");  
+    }, function errorCallback(response) {
+        $scope.offers = [];
+        console.log("nie ma takiego kanału");
+    });
 }]);
